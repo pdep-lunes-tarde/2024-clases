0423f7aa345060a9713b66bd8a6cfcb49d86bdf3
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2024-03-26 11:54:53 -0300

    Subido codigo de clase 1

--- clase-01/intro/src/Library.hs
+++ clase-01/intro/src/Library.hs
@@ -6,16 +6,45 @@ import PdePreludat
 -- Un año es bisiesto si es divisible por 400 o
 -- si es divisible por 4 pero no por 100.
 
+-- REPL
+
+-- READ
+-- EVALUATE
+-- PRINT
+-- LOOP
+
+esBisiesto :: Number -> Bool
+esBisiesto anio =
+    divide anio 400 || divide anio 4 && not (divide anio 100)
+
+divide :: Number -> Number -> Bool
+divide dividendo divisor = mod dividendo divisor == 0
+
+anioActualEsBisiesto :: Bool
+anioActualEsBisiesto = esBisiesto anioActual
+
+anioActual :: Number
+anioActual = 2024
+
+
+
+
+
+
+
+
 -- Declaratividad y expresividad
 
--- int f(int a[], int b) {
---   int i, j = 0;
+-- int cuantosPares(int numeros[], int tamanio) {
+--   int i, contadorDePares = 0;
 
---   for (i=0; i < b; i++) {
---     if(a[i] % 2 == 0){
---       j = j + 1;
+--   for (i=0; i < tamanio; i++) {
+--     if(esPar(numeros[i])){
+--       contadorDePares = contadorDePares + 1;
 --     }
 --   }
 
---   return j;
+--   return contadorDePares;
 -- }
+
+cuantosPares = length . filter even
--- clase-01/intro/src/Spec.hs
+++ clase-01/intro/src/Spec.hs
@@ -5,5 +5,11 @@ import Test.Hspec
 
 correrTests :: IO ()
 correrTests = hspec $ do
-    it "2+2 es 4" $ do
-        2 + 2 `shouldBe` 4
+    it "un anio es bisiesto si es divisible por 400" $ do
+        esBisiesto 800 `shouldBe` True
+    it "un anio es bisiesto si es divisible por 4 pero no por 100" $ do
+        esBisiesto 104 `shouldBe` True
+    it "un anio no es bisiesto si no es divisible por 4" $ do
+        esBisiesto 7 `shouldBe` False
+    it "un anio no es bisiesto si es divisible por 4 y 100, pero no por 400" $ do
+        esBisiesto 100 `shouldBe` False
