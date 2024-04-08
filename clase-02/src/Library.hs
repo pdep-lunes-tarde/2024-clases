9144ad0b10aec1865b7d15441bf66332d71a570b
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2024-04-08 16:53:37 -0300

    Implementada funcion preguntar en clase 2

--- clase-02/src/Library.hs
+++ clase-02/src/Library.hs
@@ -3,8 +3,26 @@ module Library (module Library, isPrefixOf, isSuffixOf) where
 import PdePreludat
 import Data.List (isPrefixOf, isSuffixOf)
 
+-- preguntar :: String -> String
+-- preguntar oracion
+--     | isPrefixOf "¿" oracion && isSuffixOf "?" oracion = oracion
+--     | not (isSuffixOf "?" oracion) && isPrefixOf "¿" oracion =oracion ++ "?"
+--     | not (isPrefixOf "¿" oracion) && isSuffixOf "?" oracion = "¿" ++ oracion 
+--     | otherwise = "¿" ++ oracion ++ "?"
+
 preguntar :: String -> String
-preguntar oracion = ""
+preguntar oracion =
+    garantizarSignoDePreguntaQueAbre (garantizarSignoDePreguntaQueCierra oracion)
+
+garantizarSignoDePreguntaQueCierra :: String -> String
+garantizarSignoDePreguntaQueCierra oracion
+    | isSuffixOf "?" oracion = oracion
+    | otherwise = oracion ++ "?"
+
+garantizarSignoDePreguntaQueAbre :: String -> String
+garantizarSignoDePreguntaQueAbre oracion
+    | isPrefixOf "¿" oracion = oracion
+    | otherwise = "¿" ++ oracion
 
 -- isPrefixOf nos dice si un string empieza con otro string
 -- isSuffixOf nos dice si un string termina con otro string
@@ -21,6 +39,6 @@ preguntar oracion = ""
 
 -- Queremos que funcione así:
 -- >>> preguntar "hola"
--- "hola?"
+-- ""
 -- >>> preguntar "hola?"
--- "hola?"
+-- ""
