8edf50fd78c999a8dd06115365d97fc69859e7d1
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2024-04-15 16:48:22 -0300

    Codigo hasta antes del mas4

--- clase-03/src/Library.hs
+++ clase-03/src/Library.hs
@@ -1,7 +1,10 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Eta reduce" #-}
 module Library where
-import PdePreludat
+
 import Data.List (isPrefixOf)
 import GHC.OldList (isSuffixOf)
+import PdePreludat
 
 ---------------------
 ------ Parte 0 ------
@@ -11,19 +14,29 @@ import GHC.OldList (isSuffixOf)
 -- Queremos tener una función que nos diga si una carta puede jugarse después de otra.
 -- Por ahora, vamos a arrancar implementando solo las cartas con números.
 
-type Carta = (Number, String)
+data Carta = UnaCarta Number Color deriving (Eq)
 
-carta :: Number -> String -> Carta
-carta numero color = (numero, color)
+instance Show Carta where
+    show (UnaCarta unNumero unColor) =
+        show unNumero ++ "--" ++ show unColor
 
-color :: Carta -> String
-color (unNumero, unColor) = unColor
+-- type Carta = (Number, String)
+-- si usamos type,  Carta == (Number, String)
+-- si usamos data, Carta != (Number, String)
+
+data Color = Rojo | Azul | Amarillo | Verde deriving (Show, Eq)
+
+carta :: Number -> Color -> Carta
+carta numero color = UnaCarta numero color
+
+color :: Carta -> Color
+color (UnaCarta unNumero unColor) = unColor
 
 tienenMismoColor :: Carta -> Carta -> Bool
 tienenMismoColor unaCarta otraCarta = color unaCarta == color otraCarta
 
 numero :: Carta -> Number
-numero (unNumero, unColor) = unNumero
+numero (UnaCarta unNumero unColor) = unNumero
 
 tienenMismoNumero :: Carta -> Carta -> Bool
 tienenMismoNumero unaCarta otraCarta = numero unaCarta == numero otraCarta
@@ -31,23 +44,30 @@ tienenMismoNumero unaCarta otraCarta = numero unaCarta == numero otraCarta
 sePuedeJugar :: Carta -> Carta -> Bool
 sePuedeJugar unaCarta otraCarta = tienenMismoColor unaCarta otraCarta || tienenMismoNumero unaCarta otraCarta
 
-
 ---------------------
 ------ Parte 1 ------
 ---------------------
 
-
 -- Hay jugadores, que tienen un nombre y una cantidad de puntos que obtuvieron
 -- por sus resultados en partidas de uno.
 -- Queremos poder pedirle su nombre y también sus puntos.
-juan = implementame
+data Jugador = UnJugador Number String deriving (Show, Eq)
 
-nombre jugador = implementame
+jugador puntos nombre = UnJugador puntos nombre
+
+juan :: Jugador
+juan = jugador 10 "Juan"
+
+nombre :: Jugador -> String
+nombre (UnJugador _ unNombre) = unNombre
+
 -- Ejemplo:
 -- nombre juan
 -- > "Juan"
 
-puntos jugador = implementame
+puntos :: Jugador -> Number
+puntos (UnJugador unosPuntos _) = unosPuntos
+
 -- Ejemplo:
 -- puntos juan
 -- > 10
@@ -59,7 +79,7 @@ puntos jugador = implementame
 -- Queremos saber entre dos jugadores, quien tiene mas puntos.
 -- Si ambos tienen la misma cantidad de puntos, me da el primero que le pasé.
 -- Nota: esto tiene que devolver un jugador.
-juani = implementame
+juani = jugador 12 "Juani"
 
 quienTieneMasPuntos unJugador otroJugador = implementame
 
@@ -68,7 +88,14 @@ quienTieneMasPuntos unJugador otroJugador = implementame
 -- si el resultado es que empató, suma 1 punto
 -- si el resultado es que perdió, suma 0 puntos
 
-trasJugarPartida jugador resultado = jugador
+sumarPuntos :: Number -> Jugador -> Jugador
+sumarPuntos cantidad unJugador = jugador (puntos unJugador + cantidad)
+                                         (nombre unJugador)
+
+trasJugarPartida :: Jugador -> String -> Jugador
+trasJugarPartida unJugador "Gano" = sumarPuntos 3 unJugador
+trasJugarPartida unJugador "Empato" = sumarPuntos 1 unJugador
+trasJugarPartida unJugador "Perdio" = unJugador
 
 ---------------------
 ------ Parte 3 ------
--- clase-03/src/Spec.hs
+++ clase-03/src/Spec.hs
@@ -3,10 +3,10 @@ import PdePreludat
 import Library
 import Test.Hspec
 
-ochoAzul = (8, "Azul")
-ochoRojo = (8, "Rojo")
-sieteAzul = (7, "Azul")
-sieteVerde = (7, "Verde")
+ochoAzul = carta 8 Azul
+ochoRojo = carta 8 Rojo
+sieteAzul = carta 7 Azul
+sieteVerde = carta 7 Verde
 
 
 correrTests :: IO ()
@@ -20,9 +20,9 @@ correrTests = hspec $ do
       it "una carta se puede jugar despues de otra si coinciden sus colores" $ do
         sePuedeJugar ochoAzul sieteAzul `shouldBe` True
     describe "Jugadores" $ do
-      let tomi = implementame -- tomi se llama "Tomas" y tiene 10 puntos
-          emi = implementame -- emi se llama "Emiliano" y tiene 12 puntos
-          manu = implementame -- manu se llama "Manuel" y tiene 10 puntos
+      let tomi = jugador 10 "Tomas" -- tomi se llama "Tomas" y tiene 10 puntos
+          emi = jugador 12 "Emiliano" -- emi se llama "Emiliano" y tiene 12 puntos
+          manu = jugador 10 "Manuel" -- manu se llama "Manuel" y tiene 10 puntos
       it "nombre me devuelve el nombre de un jugador" $ do
         nombre tomi `shouldBe` "Tomas"
       it "puntos me devuelve los puntos de un jugador" $ do
@@ -38,15 +38,15 @@ correrTests = hspec $ do
       --     quienTieneMasPuntos tomi manu `shouldBe` tomi
       --     quienTieneMasPuntos manu tomi `shouldBe` manu
       describe "trasJugarPartida" $ do
-        let partidaGanada = implementame
-            partidaPerdida = implementame
-            partidaEmpatada = implementame
+        let partidaGanada = "Gano"
+            partidaPerdida = "Perdio"
+            partidaEmpatada = "Empato"
         it "dado un jugador y una partida ganada, aumenta en 3 los puntos del jugador" $ do
           puntos (trasJugarPartida tomi partidaGanada) `shouldBe` 13
         it "dado un jugador y una partida perdida, deja al jugador con los mismos puntos que tenia" $ do
           puntos (trasJugarPartida tomi partidaPerdida) `shouldBe` 10
         it "dado un jugador y una partida empatada, aumenta en 1 los puntos del jugador" $ do
-          puntos (trasJugarPartida tomi partidaPerdida) `shouldBe` 11
+          puntos (trasJugarPartida tomi partidaEmpatada) `shouldBe` 11
     describe "Cartas de mas 4" $ do
       let mas4Rojo = implementame
       it "siempre se puede jugar una carta de mas 4 por sobre otra carta" $ do
