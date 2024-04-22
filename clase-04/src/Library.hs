95155429fca26e535e2cc094c6c890ee6101e14f
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2024-04-22 10:57:31 -0300

    Codigo inicial clase de recursividad y listas

---
+++ clase-04/src/Library.hs
@@ -0,0 +1,28 @@
+module Library where
+import PdePreludat
+
+-- Recursividad
+
+-- Listas
+
+-- primero [1,2,3] deberia ser 1
+primero = implementame
+
+-- ultimo ["hola", "que", "tal"] deberia ser "tal"
+ultimo = implementame
+
+-- cantidad ["hola", "que", "tal"] deberia ser 3
+cantidad = implementame
+
+-- tiene 3 [1,2,3] deberia ser True
+-- tiene 4 [1,2,3] deberia ser False
+tiene = implementame
+
+-- agregar 1 [1,2,3] deberia ser [1,1,2,3]
+agregar = implementame
+
+-- agregarAlFinal 1 [1,2,3] deberia ser [1,2,3,1]
+agregarAlFinal = implementame
+
+-- agregarTodos [1,2,3] [4,5,6] = [1,2,3,4,5,6]
+agregarTodos = implementame
---
+++ clase-04/src/Spec.hs
@@ -0,0 +1,47 @@
+module Spec where
+import PdePreludat
+import Library
+import Test.Hspec
+import Control.Exception (evaluate)
+import Test.Hspec.Runner (Config(..), defaultConfig, hspecWith)
+import Test.Hspec.Formatters (specdoc)
+
+correrTests :: IO ()
+correrTests = runHspec $ do
+  describe "Primero" $ do
+    it "Si la lista tiene elementos, me devuelve el primero" $ do
+      primero [1,2,3] `shouldBe` 1
+    it "Si la lista no tiene elementos, deberia fallar" $ do
+      deberiaFallar (primero [])
+  describe "Ultimo" $ do
+    it "Si la lista tiene elementos, me devuelve el ultimo" $ do
+      ultimo [1,2,3] `shouldBe` 3
+    it "Si la lista no tiene elementos, deberia fallar" $ do
+      deberiaFallar (ultimo [])
+  describe "Cantidad" $ do
+    it "Si la lista no tiene elementos, es 0" $ do
+      cantidad [] `shouldBe` 0
+    it "Si la lista tiene elementos, es la cantidad de elementos en la lista" $ do
+      cantidad ["uno", "dos", "tres", "cuatro"] `shouldBe` 4
+  describe "Tiene" $ do
+    it "Si el elemento esta contenido en la lista, es True" $ do
+      tiene "hola" ["hola", "mundo!"] `shouldBe` True
+    it "Si el elemento no esta contenido en la lista, es False" $ do
+      tiene "pdep" ["hola", "mundo!"] `shouldBe` False
+  describe "agregar" $ do
+    it "Devuelve la lista con el elemento agregado al principio" $ do
+      agregar 'a' "bc" `shouldBe` "abc"
+  describe "agregarAlFinal" $ do
+    it "Devuelve la lista con el elemento agregado al principio" $ do
+      agregar 'z' "abc" `shouldBe` "abcz"
+  describe "agregarTodos" $ do
+    it "Agrega todos los elementos de la segunda lista a la primera al final de la misma" $ do
+      agregar [1,2,3] [4,5,6] `shouldBe` [1,2,3,4,5,6]
+      
+    
+
+deberiaFallar :: a -> Expectation
+deberiaFallar unaExpresion = evaluate unaExpresion `shouldThrow` anyException
+
+runHspec :: Spec -> IO ()
+runHspec tests = hspecWith defaultConfig {configFormatter = Just specdoc} tests
