8c054ebe0b9a51a9f15ee2065c8492072dfe9b16
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2024-04-22 16:53:50 -0300

    Codigo final de clase 4

--- clase-04/src/Library.hs
+++ clase-04/src/Library.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use foldr" #-}
 module Library where
 import PdePreludat
 
@@ -14,8 +16,7 @@ multiplicar a b | b < 0 = multiplicar a (b + 1) - a
 -- Tienen dos constructores
 
 -- primero [1,2,3] deberia ser 1
-primero :: [a] -> a
-primero (x : _) = x
+
 
 
 elResto :: [a] -> [a]
@@ -29,26 +30,48 @@ estaVacia _ = False
 -- estaVacia lista = lista == []
 
 -- agregar 0 [1,2,3] deberia ser [0,1,2,3]
--- [] /// Lista Vacia
--- (x : xs) /// Cabeza y Cola
+
 
 -- agregar elemento (x : xs) = elemento : x : xs
 -- agregar elemento [] = elemento : []
 agregar :: a -> [a] -> [a]
 agregar elemento lista = elemento : lista
 
--- ultimo ["hola", "que", "tal"] deberia ser "tal"
-ultimo = implementame
+primero :: [a] -> a
+primero (x : _) = x
 
+-- ultimo ["hola", "que", "tal"] deberia ser "tal"
+ultimo :: [a] -> a
+ultimo (x : []) = x
+-- ultimo [x] = x
+ultimo (x : xs) = ultimo xs
+
+-- [] /// Lista Vacia
+-- (x : xs) /// Cabeza y Cola
 -- cantidad ["hola", "que", "tal"] deberia ser 3
-cantidad = implementame
+cantidad :: [a] -> Number
+cantidad [] = 0
+cantidad (x : xs) = 1 + cantidad xs
 
 -- tiene 3 [1,2,3] deberia ser True
 -- tiene 4 [1,2,3] deberia ser False
-tiene = implementame
+tiene :: Eq a => a -> [a] -> Bool
+tiene elemento [] = False
+tiene elemento (primero : resto) =
+    elemento == primero || tiene elemento resto
 
 -- agregarAlFinal 1 [1,2,3] deberia ser [1,2,3,1]
-agregarAlFinal = implementame
+agregarAlFinal :: a -> [a] -> [a]
+agregarAlFinal elemento [] = [elemento]
+agregarAlFinal elemento (x : xs) =
+    x : agregarAlFinal elemento xs
 
 -- agregarTodos [1,2,3] [4,5,6] = [1,2,3,4,5,6]
-agregarTodos = implementame
+agregarTodos :: [a] -> [a] -> [a]
+agregarTodos [] unaLista = unaLista
+agregarTodos unaLista [] = unaLista
+agregarTodos unaLista (primero:resto) =
+    agregarTodos (agregarAlFinal primero unaLista) resto
+
+agregarTodos unaLista otraLista =
+    unaLista ++ otraLista
