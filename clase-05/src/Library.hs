b98e7e7ad3124c6bf952342a4387323c0b10feae
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2024-04-29 16:22:22 -0300

    Clase 5 despues del filter

--- clase-05/src/Library.hs
+++ clase-05/src/Library.hs
@@ -32,19 +32,39 @@ squirtle :: Pokemon
 squirtle = Pokemon Agua 1000
 
 esDeTipoElectrico :: Pokemon -> Bool
-esDeTipoElectrico pokemon = esDeTipo pokemon Electrico
+esDeTipoElectrico pokemon = esDeTipo Electrico pokemon 
 
 esDeTipoAgua :: Pokemon -> Bool
-esDeTipoAgua pokemon = esDeTipo pokemon Agua
+esDeTipoAgua pokemon = esDeTipo Agua pokemon 
 
 esDeTipoFuego :: Pokemon -> Bool
-esDeTipoFuego pokemon = esDeTipo pokemon Fuego
-
-esDeTipo :: Pokemon -> Tipo -> Bool
-esDeTipo pokemon unTipo = tipo pokemon == unTipo
+esDeTipoFuego pokemon = esDeTipo Fuego pokemon 
 
 esFuerte :: Pokemon -> Bool
 esFuerte pokemon = experiencia pokemon >= 1000
 
 esRaro :: Pokemon -> Bool
 esRaro pokemon = esDeTipoFuego pokemon && esFuerte pokemon
+
+-- 2) a)
+
+-- filter
+-- seleccionar :: (a -> Bool) -> [a] -> [a]
+-- seleccionar condicion [] = []
+-- seleccionar condicion (x : xs)
+--     | condicion x = x : seleccionar  condicion xs
+--     | otherwise = seleccionar condicion xs 
+
+losFuertes :: [Pokemon] -> [Pokemon]
+losFuertes pokemons = filter esFuerte pokemons
+
+losRaros :: [Pokemon] -> [Pokemon]
+losRaros pokemones = filter esRaro pokemones
+
+losDeTipo :: Tipo -> [Pokemon] -> [Pokemon]
+losDeTipo unTipo pokemones =
+    filter (esDeTipo unTipo) pokemones
+--                                  ^ Pokemon -> Bool
+-- esDeTipo
+esDeTipo :: Tipo -> Pokemon -> Bool
+esDeTipo unTipo pokemon = tipo pokemon == unTipo
