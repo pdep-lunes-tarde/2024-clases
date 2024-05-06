2b35bcd086bf6ac029a15028d84c8e2c4f0c3e0f
Juani Schuhmann <76596375+jschuhmann47@users.noreply.github.com>
2024-05-05 22:16:43 -0300

    Update Library.hs

--- clase-06/src/Library.hs
+++ clase-06/src/Library.hs
@@ -1,3 +1,49 @@
 module Library where
 import PdePreludat
 
+data CajaRegistradora = UnaCaja{
+  cajero::String,
+  dinero::Integer
+} deriving (Show, Eq)
+
+cobrar dineroACobrar (UnaCaja unCajero unDinero) = UnaCaja unCajero (unDinero+dineroACobrar)
+
+
+-- Punto 1: cobrar dos veces a una persona por pagar en dos cuotas
+
+-- cobrarDosCuotas = implementame
+
+
+-- Punto 2: saber si la longitud del nombre del cajero es par
+-- nombreCajeroPar = implementame
+
+-- Punto 3: queremos poder cobrar tres veces a la misma persona por dos cuotas
+-- cobrarTresVecesDosCuotas = implementame
+
+
+caja1 = UnaCaja "Tomi" 75
+caja2 = UnaCaja "JuanFds" 120
+caja3 = UnaCaja "Dante" 225
+caja4 = UnaCaja "Manu" 25
+caja5 = UnaCaja "Juani" 95
+cajasDelSuper = [caja1, caja2, caja3, caja4, caja5]
+
+-- Punto 4: dada una lista de cajas del supermercado, queremos saber cuales tienen mas de 100
+-- pesos disponibles
+
+-- cajasConMasDeCienPesos = implementame
+
+-- Punto 5: queremos saber si la sumatoria de la plata en cajas es superior a
+-- cierto numero
+
+-- sumatoriaEnCajasMayorA = implementame
+
+-- Punto 6: Por ultimo, queremos quedarnos con la primer letra del nombre
+-- de la caja que tiene el cajero con nombre mas largo
+
+-- primerLetraDeCajeroConMasDinero = implementame
+
+mayorSegun :: Ord b => (a->b) -> a -> a -> a
+mayorSegun funcion valor1 valor2
+    | funcion valor1 > funcion valor2 = valor1  
+    | otherwise = valor2
