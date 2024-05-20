5582b7cc2bc461cf6fcc4dc39216220e7b214f7b
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2024-05-19 21:37:46 -0300

    Codigo inicial de clase 7

---
+++ clase-07/src/Library.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE BangPatterns #-}
+module Library where
+import PdePreludat
+
+unos :: [Number]
+unos = implementame
+
+repetir :: a -> [a]
+repetir = implementame
+
+replicar :: Number -> a -> [a]
+replicar = implementame
+
+naturales :: [Number]
+naturales = implementame
+
+losNaturalesPares :: [Number]
+losNaturalesPares = implementame
+
+losNaturalesImpares :: [Number]
+losNaturalesImpares = implementame
+
+serieDeFibonacci :: [Number]
+serieDeFibonacci = implementame
