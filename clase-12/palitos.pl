2b9c11c590713c8c9ed204ea142daa0224fa0b4c
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2024-07-01 15:35:48 -0300

    Codigo de animalDificil\2 y leGusta\2

--- clase-12/palitos.pl
+++ clase-12/palitos.pl
@@ -28,9 +28,37 @@ tiene(maiu,leon,2).
 tiene(juanDS,lagartija,1).
 tiene(feche,tiburon,1).
 
-% leGusta/2
+nadieLoTiene(Animal):-
+    animal(Animal, _, _),
+    not(tiene(_, Animal, _)).
 
+animalDificil(Animal):- 
+    nadieLoTiene(Animal).
+% Solo 1 persona tiene 1 de ese animal
+animalDificil(Animal):-
+    tiene(Persona, Animal, 1),
+    not(
+        (tiene(OtraPersona, Animal, _),
+         Persona \= OtraPersona)
+    ).
+
+leGusta(nico, Animal):-
+    animal(Animal, _, terrestre),
+    Animal \= lemur.
+leGusta(maiu, Animal):-
+    animal(Animal, Clase, _),
+    Clase \= insecto.
+leGusta(maiu, abeja).
+leGusta(juan, Animal):-
+    animal(Animal, _, acuatico).
+leGusta(juan, Animal):-
+    animal(Animal, ave, _).
+leGusta(juanR, Animal):-
+    tiene(juanR, Animal, _).
+leGusta(feche, lechuza).
 % animalDificil/1: si nadie lo tiene, o bien si una sola persona tiene uno solo.
+
+% leGusta/2
 
 % estaFeliz/1: si le gustan todos los animales que tiene.
 
@@ -39,3 +67,47 @@ tiene(feche,tiburon,1).
 % completoLaColeccion/1: si la persona tiene todos los animales.
 
 % delQueMasTiene/2: si la persona tiene más de este animal que del resto.
+
+:- begin_tests(palitos_de_la_selva).
+
+test("Un animal es dificil si nadie lo tiene", nondet):-
+    animalDificil(abeja).
+test("Un animal NO es dificil si lo tienen al menos 2 personas"):-
+    not(animalDificil(tiburon)).
+test("Un animal es dificil si solo 1 persona tiene solo 1"):-
+    animalDificil(ballena).
+test("Un animal NO es dificil si solo 1 persona lo tiene pero tiene varios de ese"):-
+    not(animalDificil(leon)).
+
+test("A nico le gustan los animales terrestres que no sean lemur"):-
+    leGusta(nico, tarantula).
+test("A nico no le gustan los animales que no son terrestres"):-
+    not(leGusta(nico, golondrina)).
+test("A nico no le gusta el lemur"):-
+    not(leGusta(nico, lemur)).
+
+test("A maiu le gustan los animales que no son insectos", nondet):-
+    leGusta(maiu, lemur).
+test("A maiu no le gustan los insectos, excepto por la abeja"):-
+    not(leGusta(maiu, tarantula)).
+test("A maiu le gusta la abeja"):-
+    leGusta(maiu, abeja).
+
+test("A juan le gustan los animales acuaticos", nondet):-
+    leGusta(juan, ballena).
+test("A juan le gustan las aves"):-
+    leGusta(juan, golondrina).
+test("A juan no le gustan los animales que no son ni aves ni acuaticos"):-
+    not(leGusta(juan, lemur)).
+
+test("A juanR le gustan los animales que tiene", nondet):-
+    leGusta(juanR, tiburon).
+test("a juanR no le gustan los animales que no tiene"):-
+    not(leGusta(juanR, puma)).
+
+test("A feche le gustan las lechuzas"):-
+    leGusta(feche, lechuza).
+test("A feche no le gustan los animales que no sean lechuzas"):-
+    not(leGusta(feche, puma)).
+
+:- end_tests(palitos_de_la_selva).
